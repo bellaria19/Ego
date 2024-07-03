ad35353043edf080f69a917cbe1aa3613915e025
bellaria19 <52782842+bellaria19@users.noreply.github.com>
2024-07-03 13:37:57 +0900

    Add Report Screen

---
+++ lib/screens/history/monthly_report_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MonthlyReportScreen extends StatelessWidget {
+  const MonthlyReportScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('This is the Monthly Report screen.'),
+      ),
+    );
+  }
+}
