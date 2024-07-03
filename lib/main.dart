a73afa9b8ee906a1da47312aa78ef805c98f475e
bellaria19 <52782842+bellaria19@users.noreply.github.com>
2024-07-03 12:21:32 +0900

    Add a Supabase to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,37 @@
 import 'package:flutter/material.dart';
-import 'app.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:ego/screens/auth/splash_screen.dart';
+import 'package:ego/utils/constants.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+Future<void> main() async {
+// void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load(fileName: '.env');
+
+  await Supabase.initialize(
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
+  );
+
   runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'EGO',
+      theme: appTheme,
+      home: const SplashScreen(),
+      // initialRoute: '/',
+      // routes: {
+      //   '/': (context) => const SplashScreen(),
+      // '/signIn': (context) => const AuthScreen(),
+      // },
+    );
+  }
 }
