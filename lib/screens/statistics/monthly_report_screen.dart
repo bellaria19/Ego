3ccde5e02121228ebaf6a1557e3027052cd9a9f5
bellaria19 <52782842+bellaria19@users.noreply.github.com>
2024-07-13 19:28:57 +0900

    Add dummy text

--- lib/screens/statistics/monthly_report_screen.dart
+++ lib/screens/statistics/monthly_report_screen.dart
@@ -5,9 +5,28 @@ class MonthlyReportScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          '월간 리포트',
+        ),
+      ),
       body: Center(
-        child: Text('This is the Monthly Report screen.'),
+        child: Container(
+          padding: const EdgeInsets.all(1.0),
+          decoration: BoxDecoration(
+            color: Colors.yellow,
+            border: Border.all(
+              color: Colors.black,
+              width: 2.0,
+            ),
+          ),
+          child: Column(
+            children: [
+              Text('월간 리포트 화면입니다.'),
+            ],
+          ),
+        ),
       ),
     );
   }
