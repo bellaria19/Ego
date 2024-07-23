0f0432dd1cb3acc6104ab8e4d916ecd789c12d04
bellaria19 <52782842+bellaria19@users.noreply.github.com>
2024-07-24 04:35:40 +0900

    Edit ThemeData

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -29,30 +29,11 @@ final appTheme = ThemeData.light().copyWith(
       fontSize: 18,
     ),
   ),
+  scaffoldBackgroundColor: Colors.white,
   primaryColor: Colors.orange,
   textButtonTheme: TextButtonThemeData(
     style: TextButton.styleFrom(
       foregroundColor: Colors.orange,
-    ),
-  ),
-  inputDecorationTheme: InputDecorationTheme(
-    floatingLabelStyle: const TextStyle(
-      color: Colors.orange,
-    ),
-    border: OutlineInputBorder(
-      borderRadius: BorderRadius.circular(12),
-      borderSide: const BorderSide(
-        color: Colors.grey,
-        width: 2,
-      ),
-    ),
-    focusColor: Colors.orange,
-    focusedBorder: OutlineInputBorder(
-      borderRadius: BorderRadius.circular(12),
-      borderSide: const BorderSide(
-        color: Colors.orange,
-        width: 2,
-      ),
     ),
   ),
 );
